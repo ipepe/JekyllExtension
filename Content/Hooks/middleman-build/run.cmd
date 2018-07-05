d44363ce564dd53fe83f034793c3d56d3fdab0de
Patryk Ptasinski <github@ipepe.pl>
2018-07-05 03:12:55 +0200

    Fixed run.cmd. Version 0.0.4

--- Content/Hooks/middleman-build/run.cmd
+++ Content/Hooks/middleman-build/run.cmd
@@ -1,3 +1,6 @@
+SET PATH=%PATH%;%HOME%\SiteExtensions\MiddlemanExtension\Commands\Ruby-2.3.3\ruby-2.3.3-i386-mingw32\bin
 cd %HOME%/site/repository
-call bundle install --without=development,test
+echo Installing dependencies
+call bundle install --without=development,test --deployment
+echo Building middleman
 call bundle exec middleman build --build-dir="%HOME%/site/wwwroot"
